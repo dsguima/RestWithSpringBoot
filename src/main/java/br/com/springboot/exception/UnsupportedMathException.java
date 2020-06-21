42895f9c1f4ecda1ce8edb0bf2aac15f83add47b
daniele guimarães <daniele.guimaraes@solutis.com.br>
2020-06-21 12:14:26 -0300

    calc

---
+++ src/main/java/br/com/springboot/exception/UnsupportedMathException.java
@@ -0,0 +1,18 @@
+package br.com.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UnsupportedMathException(String exception) {
+		super(exception);
+	}
+
+}
